12117652ce0930ad0628e0f48579c2678100d7b1
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2020-10-02 09:38:47 -0300

    Refatoramento concluido -API funcionando GET - 02/10/2020 as 9:38

--- connection/sqlQuery.js
+++ connection/sqlQuery.js
@@ -2,30 +2,11 @@ const { poolPromise } = require('./db')
 
 async function sqlQuery( sSQL, fn1 ) {      
     try {  
-        const pool = await poolPromise 
-        const result = await pool.request()  
-        .query( sSQL ,function(err, profileset){  
-            
-            console.log('sqlQuery',1000)
-
-            if (err) {  
-
-                console.log('sqlQuery ERRO:',err)
-
-                fn1( { "Erro" : err }  )
-
-                return { "Erro" : err }
-
-            } else { 
-                var resultado = profileset.recordset; 
-                console.log('sqlQuery',resultado,sSQL)
-                fn1(resultado)
-                return resultado
-                //pool.close  
-            }  
-        })  
-        } catch (err) {  
-            fn1( { "Erro" : err.message } )
-        } 
+        let pool = await poolPromise 
+        let result = await pool.request().query( sSQL )
+        return result.recordset
+    } catch (err) {  
+        return ( { "Erro" : err.message } )
+    } 
 }
 module.exports = sqlQuery
--- consultas/apiCliente.js
+++ consultas/apiCliente.js
@@ -1,6 +1,8 @@
 const sqlQuery = require('../connection/sqlQuery')
 
-//                   TESTE : parametros = ["16851732000206","43673","2"]
+//  TESTE : parametros = ["16851732000206","43673","2"]
+//  http://localhost:5000/apicliente?cnpj=16851732000206&documento=43673&serie=2
+
 var retorno = {}
 var wemp, wctrc, wcnhserie, wwhere, wtrecho, wunidest, wcnpjentrega, wchave
 var wcnpj, wnf, wnfserie
@@ -21,13 +23,15 @@ async function apiCliente( req, res ) {
   retorno.destinoPrestacao = {}
   retorno.localEntrega     = {}
   
+  //------------------------------------ 
   if ( req.method == 'GET' ) {
      var { cnpj, documento, serie } = req.query
      wcnpj    = cnpj
      wnf      = documento
      wnfserie = serie ? serie : ''
- }
-
+  }
+  
+  //------------------------------------ 
   if ( req.method == 'POST' ) {
       var { valoresParametros } = req.body
       if (valoresParametros) {
@@ -42,79 +46,78 @@ async function apiCliente( req, res ) {
       }    
   }
   
-          set_nf()
-          //.then( set_cnh() )
-          //.then( set_trecho() )
-          //.then( set_unid_destino() )
-          //.then( set_local_entrega() )
-          //.then( set_ocorrencias() )
-          .then( (a) => { res.json(retorno).status(200) })
-          .catch( (err) => { res.send(err.message).status(500) })
+  //------------------------------------ 
+  try {
+
+      await set_nf()
+      await set_cnh() 
+      await set_trecho() 
+      await set_unid_destino() 
+      await set_local_entrega() 
+      await set_ocorrencias()
+      
+      res.json(retorno).status(200) 
+
+  } catch (err) { 
+      res.send(err.message).status(500) 
+  }  
 }
 
 async function set_nf() {
-  sqlQuery(`
+  let data = await sqlQuery(`
     SELECT EMP_CODIGO AS CNH_EMPRESA,CNH_SERIE,CNH_CTRC,NF,SERIE,DATA,VALOR,CHAVENFE  
     FROM NFR
     WHERE
     SERIE='${wnfserie}' AND NF=${wnf} AND CLI_CGCCPF_REMET=${wcnpj}
-    `, 
-             ( data ) => {
-                retorno.numero          = data[0].CNH_CTRC
-                retorno.dataEmissao     = data[0].DATA
-                retorno.valorMercadoria = data[0].VALOR
-              //------------------------------------
-              retorno.notaFiscal.numero          = data[0].NF
-              retorno.notaFiscal.serie           = data[0].SERIE
-              retorno.notaFiscal.dataEmissao     = data[0].DATA
-              retorno.notaFiscal.valor           = data[0].VALOR
-              retorno.notaFiscal.chaveNFe        = data[0].CHAVENFE
-              //------------------------------------
-                wemp      = data[0].CNH_EMPRESA
-                wcnhserie = data[0].CNH_SERIE
-                wctrc     = data[0].CNH_CTRC
-              //------------------------------------
-              console.log('NF---->',wemp,wcnhserie,wctrc)
-            }
-    ).then(a => {
-      
-        console.log('XXXXXXXXXXXXXX====>>',a)
-
-        if (retorno.numero  === 0) {
-          wwhere=`NF='${wnf}' AND CLI_CGCCPF_REMET=${wcnpj}`
-        } else {
-          wwhere=`EMP_CODIGO='${wemp}' AND SERIE='${wcnhserie}' and CTRC=${wctrc} ` 
-        }
-        console.log('set_nf',retorno)
-   })
+    `)
+  //------------------------------------
+    retorno.numero          = data[0].CNH_CTRC
+    retorno.dataEmissao     = data[0].DATA
+    retorno.valorMercadoria = data[0].VALOR
+  //------------------------------------
+    retorno.notaFiscal.numero          = data[0].NF
+    retorno.notaFiscal.serie           = data[0].SERIE
+    retorno.notaFiscal.dataEmissao     = data[0].DATA
+    retorno.notaFiscal.valor           = data[0].VALOR
+    retorno.notaFiscal.chaveNFe        = data[0].CHAVENFE
+  //------------------------------------
+    wemp      = data[0].CNH_EMPRESA
+    wcnhserie = data[0].CNH_SERIE
+    wctrc     = data[0].CNH_CTRC
+  //------------------------------------
 }
 
 async function set_cnh() {
-  sqlQuery(`
-    SELECT *  
-    FROM CNH
-    WHERE  ${wwhere} 
-    `, 
-            ( data ) => {
-                retorno.numero           = data[0].CTRC
-                retorno.dataEmissao      = data[0].DATA 
-                retorno.prevEntrega      = data[0].PREVENTREGA 
-                retorno.tipoPesoM3       = data[0].ESP_CODIGO
-                retorno.pesoM3           = data[0].VOLUME
-                retorno.valorMercadoria  = data[0].VALORNF
-                retorno.valorFrete       = data[0].TOTFRETE
-                retorno.chave            = data[0].CHAVECTE
+    if (retorno.numero  === 0) {
+        wwhere=`NF='${wnf}' AND CLI_CGCCPF_REMET=${wcnpj}`
+    } else {
+        wwhere=`EMP_CODIGO='${wemp}' AND SERIE='${wcnhserie}' and CTRC=${wctrc} ` 
+    }   
 
-                wtrecho       = data[0].TRE_CODIGO
-                wcnpjentrega  = data[0].CLI_CGCCPF_DEST
-               //--------------------------------------------
-                wchave = `${wemp}${wcnhserie}${wctrc}` 
-            }
-    )
+    data = await sqlQuery(`
+      SELECT *  
+      FROM CNH
+      WHERE  ${wwhere} 
+    `)
+
+    //------------------------------------
+    retorno.numero           = data[0].CTRC
+    retorno.dataEmissao      = data[0].DATA 
+    retorno.prevEntrega      = data[0].PREVENTREGA 
+    retorno.tipoPesoM3       = data[0].ESP_CODIGO
+    retorno.pesoM3           = data[0].VOLUME
+    retorno.valorMercadoria  = data[0].VALORNF
+    retorno.valorFrete       = data[0].TOTFRETE
+    retorno.chave            = data[0].CHAVECTE
+    //--------------------------------------------
+    wtrecho       = data[0].TRE_CODIGO
+    wcnpjentrega  = data[0].CLI_CGCCPF_DEST
+    //--------------------------------------------
+    wchave = `${wemp}${wcnhserie}${wctrc}` 
 }
 
 async function set_trecho() {
-  sqlQuery(`
+  data = await sqlQuery(`
     SELECT TRE.CODIGO   AS TRECHO
         ,TRE.ORIGEM     AS TRECHO_ORIGEM
         ,TRE.DESTINO    AS TRECHO_DESTINO
@@ -129,25 +132,21 @@ async function set_trecho() {
     LEFT JOIN CID ORIGEM ON ORIGEM.CODIGO = SUBSTRING(TRE.CODIGO, 1, 3)
     LEFT JOIN CID DESTINO ON DESTINO.CODIGO = SUBSTRING(TRE.CODIGO, 4, 3)
     WHERE TRE.CODIGO = '${wtrecho}'
-    `, 
-            ( data ) => {
-                
-              console.log(data)
-
-                retorno.origemPrestacao.nome  = data[0].CIDADE_ORIGEM
-                retorno.origemPrestacao.uf    = data[0].UF_ORIGEM
-                retorno.origemPrestacao.ibge  = data[0].IBGE_ORIGEM
-                retorno.destinoPrestacao.nome = data[0].CIDADE_DESTINO
-                retorno.destinoPrestacao.uf   = data[0].UF_DESTINO
-                retorno.destinoPrestacao.ibge = data[0].IBGE_DESTINO
-               //------------------------------------
-                wunidest = data[0].UNID_DESTINO
-            }
-   )
+  `)
+    
+  //------------------------------------
+    retorno.origemPrestacao.nome  = data[0].CIDADE_ORIGEM
+    retorno.origemPrestacao.uf    = data[0].UF_ORIGEM
+    retorno.origemPrestacao.ibge  = data[0].IBGE_ORIGEM
+    retorno.destinoPrestacao.nome = data[0].CIDADE_DESTINO
+    retorno.destinoPrestacao.uf   = data[0].UF_DESTINO
+    retorno.destinoPrestacao.ibge = data[0].IBGE_DESTINO
+    //------------------------------------
+    wunidest = data[0].UNID_DESTINO
 }
 
 async function set_unid_destino() {
-  sqlQuery(`
+  data = await sqlQuery(`
         SELECT EMP.CODIGO,  
           EMP.NOME,    
           EMP.ENDERECO,
@@ -159,68 +158,60 @@ async function set_unid_destino() {
         FROM EMP
         LEFT JOIN CID ON CID.CODIGO = EMP.CID_CODIGO
         WHERE EMP.CODIGO = '${wunidest}'
-  `, 
-          ( data ) => {
-            retorno.unidadeDestino.sigla        = data[0].CODIGO   
-            retorno.unidadeDestino.nome         = data[0].NOME     
-            retorno.unidadeDestino.endereco     = data[0].ENDERECO 
-            retorno.unidadeDestino.numero       = data[0].NUMERO   
-            retorno.unidadeDestino.bairro       = data[0].BAIRRO   
-            retorno.unidadeDestino.cidade       = {}
-            retorno.unidadeDestino.cidade.ibge  = data[0].IBGE         
-            retorno.unidadeDestino.cidade.nome  = data[0].CIDADE       
-            retorno.unidadeDestino.cidade.uf    = data[0].UF       
-           //------------------------------------
-    }
- )
+  `)
+  //------------------------------------
+    retorno.unidadeDestino.sigla        = data[0].CODIGO   
+    retorno.unidadeDestino.nome         = data[0].NOME     
+    retorno.unidadeDestino.endereco     = data[0].ENDERECO 
+    retorno.unidadeDestino.numero       = data[0].NUMERO   
+    retorno.unidadeDestino.bairro       = data[0].BAIRRO   
+    retorno.unidadeDestino.cidade       = {}
+    retorno.unidadeDestino.cidade.ibge  = data[0].IBGE         
+    retorno.unidadeDestino.cidade.nome  = data[0].CIDADE       
+    retorno.unidadeDestino.cidade.uf    = data[0].UF       
+  //------------------------------------
 }
 
 async function set_local_entrega() {
-  sqlQuery(`
+  data = await sqlQuery(`
         SELECT ENDERECO,NUMERO,BAIRRO,CID.NOME AS CIDADE,CID.UF,CID.CODMUN AS IBGE
         FROM CLI 
         LEFT JOIN CID ON CID.CODIGO = CLI.CID_CODIGO
         WHERE CGCCPF = ${wcnpjentrega}
-  `, 
-          ( data ) => {
+  `)
 
-            retorno.localEntrega.endereco    = data[0].ENDERECO
-            retorno.localEntrega.numero      = data[0].NUMERO
-            retorno.localEntrega.bairro      = data[0].BAIRRO
-            retorno.localEntrega.cidade      = {}
-            retorno.localEntrega.cidade.nome = data[0].CIDADE
-            retorno.localEntrega.cidade.uf   = data[0].UF
-            retorno.localEntrega.cidade.ibge = data[0].IBGE
-           //------------------------------------
-          }
- )
+  //------------------------------------
+    retorno.localEntrega.endereco    = data[0].ENDERECO
+    retorno.localEntrega.numero      = data[0].NUMERO
+    retorno.localEntrega.bairro      = data[0].BAIRRO
+    retorno.localEntrega.cidade      = {}
+    retorno.localEntrega.cidade.nome = data[0].CIDADE
+    retorno.localEntrega.cidade.uf   = data[0].UF
+    retorno.localEntrega.cidade.ibge = data[0].IBGE
+ //------------------------------------
 }
 
 async function set_ocorrencias() {
-  // ( teste = SPOE1480268 )
-  sqlQuery(`
+  data = await sqlQuery(`
         SELECT OUN.*, OCO.NOME AS NOMEOCORRENCIA, MOT.NOME AS MOTORISTA  
         FROM OUN  
         LEFT JOIN OCO ON OCO.CODIGO=OUN.OCO_CODIGO  
         LEFT JOIN MOT ON MOT.PRONTUARIO = OUN.MOT_PRONTUARIO  
         WHERE TABELA='CNH' AND CHAVE='${wchave}' 
         ORDER BY DATA
-  `, 
-          ( data ) => {
-            let elem
-            data.forEach((item, index)=>{
-              elem = {} 
-              elem.codigoInterno           = item.OCO_CODIGO 
-              elem.codigoProceda           = item.OCO_CODIGO 
-              elem.descricaoOcorrencia     = item.NOMEOCORRENCIA 
-              elem.dataRegistro            = item.DATAOCO       
-              retorno.ocorrencias.push(elem)  
-            })
-           //------------------------------------
-           console.log('RET : ',retorno) 
-           console.log('-----------------------------------') 
-          }
- )
+  `)
+
+  //------------------------------------  
+  let elem
+  data.forEach((item, index)=>{
+      elem = {} 
+      elem.codigoInterno           = item.OCO_CODIGO 
+      elem.codigoProceda           = item.OCO_CODIGO 
+      elem.descricaoOcorrencia     = item.NOMEOCORRENCIA 
+      elem.dataRegistro            = item.DATAOCO       
+      retorno.ocorrencias.push(elem)  
+      })
+  //------------------------------------
 }
 
 module.exports = apiCliente
