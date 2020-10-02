5246107fead3e8818d143bb29b4586d8aab371de
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2020-10-02 11:55:49 -0300

    Versão inicial API - 02/10/2020 as 11:55

--- connection/db.js
+++ connection/db.js
@@ -4,7 +4,7 @@ const config = require('../config/dbsetup.json')
 const poolPromise = new sql.ConnectionPool(config)  
     .connect()  
     .then(pool => {  
-        console.log('Conectado com sucesso !!!')  
+        console.log('BD Conectado com sucesso !!!')  
         return pool  
     })  
     .catch(err => console.log('Falha ao conectar ao BD !!!', err))  
--- consultas/apiCliente.js
+++ consultas/apiCliente.js
@@ -5,7 +5,7 @@ const sqlQuery = require('../connection/sqlQuery')
 
 var retorno = {}
 var wemp, wctrc, wcnhserie, wwhere, wtrecho, wunidest, wcnpjentrega, wchave
-var wcnpj, wnf, wnfserie
+var wcnpj, wnf, wnfserie, werror, wsqlerr
  
 async function apiCliente( req, res ) {
   retorno.numero           = 0
@@ -23,6 +23,9 @@ async function apiCliente( req, res ) {
   retorno.destinoPrestacao = {}
   retorno.localEntrega     = {}
   
+  werror  = 'apiCliente'
+  wsqlerr = ''
+
   //------------------------------------ 
   if ( req.method == 'GET' ) {
      var { cnpj, documento, serie } = req.query
@@ -59,46 +62,70 @@ async function apiCliente( req, res ) {
       res.json(retorno).status(200) 
 
   } catch (err) { 
-      res.send(err.message).status(500) 
+      res.send({ "erro" : err.message, "rotina" : werror, "sql" : wsqlerr }).status(500) 
   }  
 }
 
 async function set_nf() {
+  werror = 'set_nf'
   let data = await sqlQuery(`
     SELECT EMP_CODIGO AS CNH_EMPRESA,CNH_SERIE,CNH_CTRC,NF,SERIE,DATA,VALOR,CHAVENFE  
     FROM NFR
     WHERE
     SERIE='${wnfserie}' AND NF=${wnf} AND CLI_CGCCPF_REMET=${wcnpj}
     `)
-  //------------------------------------
-    retorno.numero          = data[0].CNH_CTRC
-    retorno.dataEmissao     = data[0].DATA
-    retorno.valorMercadoria = data[0].VALOR
-  //------------------------------------
-    retorno.notaFiscal.numero          = data[0].NF
-    retorno.notaFiscal.serie           = data[0].SERIE
-    retorno.notaFiscal.dataEmissao     = data[0].DATA
-    retorno.notaFiscal.valor           = data[0].VALOR
-    retorno.notaFiscal.chaveNFe        = data[0].CHAVENFE
-  //------------------------------------
-    wemp      = data[0].CNH_EMPRESA
-    wcnhserie = data[0].CNH_SERIE
-    wctrc     = data[0].CNH_CTRC
-  //------------------------------------
+
+   let { Erro } = data
+   if (Erro) { 
+        wsqlerr = Erro 
+        retorno.numero              = 0
+        retorno.notaFiscal.numero   = wnf
+        retorno.notaFiscal.serie    = wnfserie
+   } else {
+      //------------------------------------
+        retorno.numero          = data[0].CNH_CTRC
+        retorno.dataEmissao     = data[0].DATA
+        retorno.valorMercadoria = data[0].VALOR
+      //------------------------------------
+        retorno.notaFiscal.numero          = data[0].NF
+        retorno.notaFiscal.serie           = data[0].SERIE
+        retorno.notaFiscal.dataEmissao     = data[0].DATA
+        retorno.notaFiscal.valor           = data[0].VALOR
+        retorno.notaFiscal.chaveNFe        = data[0].CHAVENFE
+      //------------------------------------
+        wemp      = data[0].CNH_EMPRESA
+        wcnhserie = data[0].CNH_SERIE
+        wctrc     = data[0].CNH_CTRC
+      //------------------------------------
+   }
 }
 
 async function set_cnh() {
-    if (retorno.numero  === 0) {
-        wwhere=`NF='${wnf}' AND CLI_CGCCPF_REMET=${wcnpj}`
+    werror = 'set_cnh'
+    if (retorno.numero  === 0) { 
+        wwhere=`CNH.NF='${wnf}' AND CNH.CLI_CGCCPF_REMET=${wcnpj}`
     } else {
-        wwhere=`EMP_CODIGO='${wemp}' AND SERIE='${wcnhserie}' and CTRC=${wctrc} ` 
+        wwhere=`CNH.EMP_CODIGO='${wemp}' AND CNH.SERIE='${wcnhserie}' and CNH.CTRC=${wctrc} ` 
     }   
 
     data = await sqlQuery(`
-      SELECT *  
+      SELECT CNH.*,
+            NFR.SERIE    AS NF_SERIE,
+            NFR.DATA     AS NF_EMISSAO,
+            NFR.CHAVENFE AS NF_CHAVE        
       FROM CNH
+      LEFT JOIN NFR ON NFR.EMP_CODIGO = CNH.EMP_CODIGO 
+                   AND NFR.CNH_SERIE  = CNH.SERIE 
+                   AND NFR.CNH_CTRC   = CNH.CTRC
       WHERE  ${wwhere} 
     `)
+
+    if (retorno.numero  === 0) {
+        retorno.notaFiscal.serie           = data[0].NF_SERIE 
+        retorno.notaFiscal.dataEmissao     = data[0].NF_EMISSAO
+        retorno.notaFiscal.valor           = data[0].VALORNF
+        retorno.notaFiscal.chaveNFe        = data[0].NF_CHAVE
+    }
 
     //------------------------------------
     retorno.numero           = data[0].CTRC
@@ -117,6 +144,7 @@ async function set_cnh() {
 }
 
 async function set_trecho() {
+  werror = 'set_trecho'
   data = await sqlQuery(`
     SELECT TRE.CODIGO   AS TRECHO
         ,TRE.ORIGEM     AS TRECHO_ORIGEM
@@ -146,6 +174,7 @@ async function set_trecho() {
 }
 
 async function set_unid_destino() {
+  werror = 'set_unid_destino'
   data = await sqlQuery(`
         SELECT EMP.CODIGO,  
           EMP.NOME,    
@@ -173,25 +202,32 @@ async function set_unid_destino() {
 }
 
 async function set_local_entrega() {
+  werror = 'set_local_entrega'
   data = await sqlQuery(`
         SELECT ENDERECO,NUMERO,BAIRRO,CID.NOME AS CIDADE,CID.UF,CID.CODMUN AS IBGE
         FROM CLI 
         LEFT JOIN CID ON CID.CODIGO = CLI.CID_CODIGO
-        WHERE CGCCPF = ${wcnpjentrega}
+        WHERE CGCCPF = '${wcnpjentrega}'
   `)
 
-  //------------------------------------
-    retorno.localEntrega.endereco    = data[0].ENDERECO
-    retorno.localEntrega.numero      = data[0].NUMERO
-    retorno.localEntrega.bairro      = data[0].BAIRRO
-    retorno.localEntrega.cidade      = {}
-    retorno.localEntrega.cidade.nome = data[0].CIDADE
-    retorno.localEntrega.cidade.uf   = data[0].UF
-    retorno.localEntrega.cidade.ibge = data[0].IBGE
- //------------------------------------
+  let { Erro } = data
+  if (Erro) { 
+       wsqlerr = Erro 
+  } else {
+      //------------------------------------
+        retorno.localEntrega.endereco    = data[0].ENDERECO
+        retorno.localEntrega.numero      = data[0].NUMERO
+        retorno.localEntrega.bairro      = data[0].BAIRRO
+        retorno.localEntrega.cidade      = {}
+        retorno.localEntrega.cidade.nome = data[0].CIDADE
+        retorno.localEntrega.cidade.uf   = data[0].UF
+        retorno.localEntrega.cidade.ibge = data[0].IBGE
+    //------------------------------------
+  }
 }
 
 async function set_ocorrencias() {
+  werror = 'set_ocorrencias'
   data = await sqlQuery(`
         SELECT OUN.*, OCO.NOME AS NOMEOCORRENCIA, MOT.NOME AS MOTORISTA  
         FROM OUN  
@@ -210,7 +246,7 @@ async function set_ocorrencias() {
       elem.descricaoOcorrencia     = item.NOMEOCORRENCIA 
       elem.dataRegistro            = item.DATAOCO       
       retorno.ocorrencias.push(elem)  
-      })
+  })
   //------------------------------------
 }
 
